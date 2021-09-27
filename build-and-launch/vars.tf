3a22a96edc0cad905019c312908915a4481e50dc
Matheus <matheus.santana95@gmail.com>
2021-09-27 10:00:21 -0300

    add correct vars.tf

---
+++ build-and-launch/vars.tf
@@ -0,0 +1,3 @@
+variable "AWS_Access_Key" { default = "AKIA3Y45YLCBZVRKGRDY" }
+variable "AWS_Secret_Key" { default = "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" }
+variable "AWS_Region" { default = "us-east-2" }
